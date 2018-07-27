26995357334f3b83a42f04a4e391dfbb1d814fa8
Charlie-Zheng <charlie.x.3000@gmail.com>
2018-07-27 10:25:07 -0600

    Fixed Map.MoveHero(), modified friendlyHeroes.txt to spwan 2 friendlies

--- ArenaSim/src/main/Map.java
+++ ArenaSim/src/main/Map.java
@@ -265,8 +265,7 @@ public class Map {
 	 * @param newY
 	 */
 	public void moveHero(int x, int y, int newX, int newY) {
-		if (x != newX || y != newY) { 
-			//&& unitMap[y][x] != null && unitMap[newY][newX] != null) { // Move only if different
+		if (x != newX || y != newY  && unitMap[y][x] != null && unitMap[newY][newX] == null) { // Move only if different
 		//}
 																								// locations
 
